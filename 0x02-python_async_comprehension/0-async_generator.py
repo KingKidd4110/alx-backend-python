10820b0d1d894aa33588ace38dcb1118280ce9df
KingKidd4110 <ayiendabrian@gmail.com>
2023-05-05 19:47:12 +0300

    random generator refactoring

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,13 +2,14 @@
 """ async_generator co-routine """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> None:
+async def async_generator() -> Generator[float, None, None]:
     """
     The coroutine takes no arguments
-    loops = 10 asyncronously
+    Generator loop 10 times asyncronously
     """
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
