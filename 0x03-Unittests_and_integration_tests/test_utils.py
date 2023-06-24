eaf86d72360886c99cbf30a8525a618fca83f9cc
KingKidd4110 <ayiendabrian@gmail.com>
2023-06-24 23:57:51 +0300

    1-7

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
+""" Test util """
+import unittest
+import requests
+from utils import access_nested_map, get_json, memoize
+from typing import Mapping, Sequence, Any
+from unittest.mock import patch
+from parameterized import parameterized
 
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ test utils access_nested_map class """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
+        """ Test access_nested_map class """
+
+        response = access_nested_map(nested_map, path)
+        self.assertEqual(response, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """ test exception for access nested map """
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    """ Test get_json by mocking http calls """
