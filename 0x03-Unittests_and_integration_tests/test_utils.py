8c30f72654c52a4ef996d8790a1ac5e7378c5724
KingKidd4110 <ayiendabrian@gmail.com>
2023-06-25 00:14:25 +0300

    parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -36,3 +36,32 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """ Test get_json by mocking http calls """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, moke_requests_get):
+        """ test get_json output """
+        moke_requests_get.return_value.json.return_value = test_payload
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        moke_requests_get.assert_called_once_with(test_url)
+
+class TestMemoize(unittest.TestCase):
+    """ Test the memoization decorator, memoize """
+    def test_memoize(self):
+        """ Test that utils.memoize decorator """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
